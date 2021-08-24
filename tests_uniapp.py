139e670f2ffc7cea246554f74866424d2f085c1c
Lydu7979 <ludovic19sanne@yahoo.fr>
2021-08-24 17:05:08 +0200

    nv

--- tests_uniapp.py
+++ tests_uniapp.py
@@ -5,6 +5,7 @@ from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.remote.remote_connection import RemoteConnection
 from time import sleep
 import requests
+from selenium.webdriver.common.keys import Keys
 
 
 @pytest.fixture(scope='class')
@@ -51,13 +52,26 @@ class Test_url(Test_api):
         self.driver.find_element_by_xpath(n_name2).send_keys("Nom_utilisateur")
         self.driver.find_element_by_xpath(p_mdp2).send_keys("Mot_de_passe")
         sleep(4)
+        self.driver.find_element_by_xpath(p_mdp2).send_keys(Keys.ENTER)
+        sleep(4)
 
-    #def test_titre_app(self):
-        #self.driver.get('http://localhost:8501/') 
-        #sleep(4)   
-        #Tit1 = '//*[@id="app-prix-production-de-tomates"]/div/span'
-        #h1_tit = self.driver.find_element_by_xpath(Tit1).text
-        #assert "tomates" in h1_tit
+        #Titre principal de l'application
+
+        
+        h1_tp = self.driver.find_element_by_css_selector("#app-prix-production-de-tomates").text
+        assert "tomates" in h1_tp
+
+        #Titre de la base de données
+
+        h1_tbdd = self.driver.find_element_by_css_selector("#base-de-donn-es-mongodb").text
+        assert "Mongodb" in h1_tbdd
+
+        #affichage base de données
+
+        self.driver.find_element_by_xpath('//*[@id="root"]/div[1]/div/div/div/div/section[2]/div/div[1]/div[7]/div/div/div[1]/div[2]/div[2]')
+
+
+    
 
         
 
