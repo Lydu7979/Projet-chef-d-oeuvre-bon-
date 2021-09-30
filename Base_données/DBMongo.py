9de147f06055e5f45ab658344f7eea8461a0740c
Lydu7979 <ludovic19sanne@yahoo.fr>
2021-09-30 20:08:20 +0200

    Update DBMongo.py

--- Base_données/DBMongo.py
+++ Base_données/DBMongo.py
@@ -4,7 +4,7 @@ import dns
 
 def get_client_mongodb(user_name = "Thmo89",psw = "Authentication "):
     
-    uri2 = "mongodb+srv://{}:{}@cluster1.mknx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority".format(user_name, psw)
+    uri2 = "mongodb+srv://{}:{}@cluster1.mknx2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&tls=true&tlsAllowInvalidCertificates=true".format(user_name, psw)
 
     client = pymongo.MongoClient(uri2)
     return client
