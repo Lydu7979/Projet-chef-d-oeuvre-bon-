b5f473788b0c15b56c0555eb55278935f248441e
Lydu7979 <ludovic19sanne@yahoo.fr>
2021-10-05 15:59:26 +0200

    rerererer

--- app2.py
+++ app2.py
@@ -131,17 +131,17 @@ if S == True:
 	
 	#st.info("Ici, le client a accès à la base de données.")
 	#DT = pd.DataFrame(Dat, columns = ['Date', 'prix moyen au kg', 'Production quantité \ntonne(s)', 'Température minimale en °C', 
-	#                           'Température maximale en °C', 'précipitations en mm','Ensoleillement en min', 'Rafales (vitesse du vent) en km/h'])
+	#                          'Température maximale en °C', 'précipitations en mm','Ensoleillement en min', 'Rafales (vitesse du vent) en km/h'])
 	#DT.rename(columns={"Production quantité \ntonne(s)": "Production quantité tonne(s)"},inplace=True)
 	#DT=DT.iloc[pd.to_datetime(DT.Date.astype(str)).argsort()]
-	#DT.to_csv('DATA/TM22.csv',index = False)                              
+	#DT.to_csv('DATA/TM23.csv',index = False)                              
 
 	#st.write("Base de données concernant le prix et la production")
 
 	#st.dataframe(Dat2)
 	#st.info("Contrairement à la première base de données, le client verra uniquement la date, avec le prix, la production et l'id.")
 
-	DATA_URL =('./DATA/TM22.csv')
+	DATA_URL =('./DATA/TM23.csv')
 	
 	
 	st.subheader("Choix du nombre de jours pour les prédictions du prix et de la production")
