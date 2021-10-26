69003a69b546878348dc6bbaf2ca6eb343d1800b
Lydu7979 <ludovic19sanne@yahoo.fr>
2021-10-26 10:51:44 +0200

    ciococo

--- Base_données/DB2sqlite.py
+++ Base_données/DB2sqlite.py
@@ -7,10 +7,13 @@ def bdd_sql():
     conn = sqlite3.connect('data.db')
 
     dat3  = pd.read_sql_query("SELECT * FROM userstable", conn)
+    dat3.reset_index(inplace=True)
+    dat3 = dat3.rename(index = str, columns = {'index':'id_users'})
     dat3['pwd'] = dat3['password'].apply(lambda x: hashlib.sha256(x.encode()).hexdigest())
     dat3 = dat3.drop(["password"], axis = 1)
     dat3 = dat3.drop_duplicates()
     dat3 = dat3.rename(index = str, columns = {'pwd':'password'})
+    dat3.to_csv('Base_données/data_users.csv',index = False) 
     
     conn.close()
     return dat3
